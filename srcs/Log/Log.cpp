9fa0c77a6ea3883f2dcdbb149663a7ebb5c96929
Benoit Pingris <benoit.pingris@epitech.eu>
2018-05-03 10:14:40 +0200

    change mutex.lock to lock_guard

--- srcs/Log/Log.cpp
+++ srcs/Log/Log.cpp
@@ -15,13 +15,13 @@ Plazza::Log::~Log()
 
 void Plazza::Log::writeLogs(const Information &info, const std::string &msg)
 {
-	g_write.lock();
+	std::lock_guard<std::mutex> lockGuard(g_write);
 	std::map<Information, std::string> convert = {
 		{PHONE_NUMBER, "Phone number has been found"},
 		{EMAIL_ADDRESS, "Email address has been found"},
 		{IP_ADDRESS, "IP address has been found"}
 	};
 	std::ofstream file("./logs/logs.txt", std::ios_base::app);
+
 	file << convert[info] << " : " <<msg << std::endl;
-	g_write.unlock();
 }
